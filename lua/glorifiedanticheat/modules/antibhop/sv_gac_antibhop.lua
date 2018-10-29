5b54c518c6f8bdc6453697bf03f1e031c686ae0e
GlorifiedPig <killingpigs123@gmail.com>
2018-10-29 22:06:02 +0200

    anti-bhop protection

---
+++ lua/glorifiedanticheat/modules/antibhop/sv_gac_antibhop.lua
@@ -0,0 +1,8 @@
+
+hook.Add("OnPlayerHitGround", "g-AC_AntiBHopDetectionScript", function( ply )
+    ply:SetVelocity( -ply:GetVelocity() / 5 )
+    
+    if( ply:GetVelocity():Length() >= 500 ) then
+        ply:SetVelocity( -ply:GetVelocity() / 3 )
+    end
+end)
