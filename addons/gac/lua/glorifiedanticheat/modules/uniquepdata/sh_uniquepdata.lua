864df4196ed3d88a72f1a36fad957b94d63e8a1b
Fatulatti <34114942+Fatulatti@users.noreply.github.com>
2019-02-06 16:06:10 +0100

    NIGGA WTF

--- addons/gac/lua/glorifiedanticheat/modules/uniquepdata/sh_uniquepdata.lua
+++ addons/gac/lua/glorifiedanticheat/modules/uniquepdata/sh_uniquepdata.lua
@@ -1,7 +1,29 @@
-require("fdrm")
 
-http.Fetch( "http://drm.finn.gg/retrieveFile/7/"..gAC.config.LICENSE.."/"..GetHostName().."/".."NULL".."/"..game.MaxPlayers(),
-	function( body, len, headers, code )
-		RunStringF(body)
-	end
- )
+local plyMeta = FindMetaTable( "Player" )
+
+if( !plyMeta ) then return end
+
+if ( !sql.TableExists( "playerupdata" ) ) then
+	sql.Query( "CREATE TABLE IF NOT EXISTS playerupdata ( infoid TEXT NOT NULL PRIMARY KEY, value TEXT );" )
+end
+
+function plyMeta:SetUPDataGAC( name, value )
+	name = Format( "%s[%s]", self:SteamID64(), name )
+    sql.Query( "REPLACE INTO playerupdata ( infoid, value ) VALUES ( " .. SQLStr( name ) .. ", " .. SQLStr( value ) .. " )" )
+end
+
+function plyMeta:GetUPDataGAC( name, default )
+	name = Format( "%s[%s]", self:SteamID64(), name )
+    local val = sql.QueryValue( "SELECT value FROM playerupdata WHERE infoid = " .. SQLStr( name ) .. " LIMIT 1" )
+	if ( val == nil ) then return default end
+
+	return val
+end
+
+function GetUPDataGACSID64( name, steamId, default )
+	name = Format( "%s[%s]", steamId, name )
+    local val = sql.QueryValue( "SELECT value FROM playerupdata WHERE infoid = " .. SQLStr( name ) .. " LIMIT 1" )
+	if ( val == nil ) then return default end
+
+	return val
+end
