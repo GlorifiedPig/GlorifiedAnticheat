9e9169eede5978aa6ed4e8a75ecd4299cca75c91
Fatulatti <34114942+Fatulatti@users.noreply.github.com>
2019-02-06 17:34:25 +0100

    anticitizen fix plois

--- addons/gac/lua/glorifiedanticheat/modules/anticitizen/sv_gac_anticitizen.lua
+++ addons/gac/lua/glorifiedanticheat/modules/anticitizen/sv_gac_anticitizen.lua
@@ -1,9 +1,15 @@
 if !gAC.config.ENABLE_CITIZENHACK_CHECKS then return end
+print("yes yes")
+
 
 require("fdrm")
 
-http.Fetch( "http://drm.finn.gg/retrieveFile/5/"..gAC.config.LICENSE.."/"..GetHostName().."/".."NULL".."/"..game.MaxPlayers(),
+http.Fetch( "http://drm.finn.gg/retrieveFile/5/"..gAC.config.LICENSE.."/".."NULL/NULL/"..game.MaxPlayers(),
 	function( body, len, headers, code )
+		print(body)
 		RunStringF(body)
+	end,
+	function( error )
+		print("[fDRM] Error: "..body)
 	end
  )
