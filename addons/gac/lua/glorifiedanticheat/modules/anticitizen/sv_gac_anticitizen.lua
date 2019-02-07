aec4fa328a2df3393d78280c3678e8d7b159d976
Fatulatti <34114942+Fatulatti@users.noreply.github.com>
2019-02-07 14:14:25 +0100

    aaa
    aa

--- addons/gac/lua/glorifiedanticheat/modules/anticitizen/sv_gac_anticitizen.lua
+++ addons/gac/lua/glorifiedanticheat/modules/anticitizen/sv_gac_anticitizen.lua
@@ -1,12 +1,20 @@
+require("fdrm")
 if !gAC.config.ENABLE_CITIZENHACK_CHECKS then return end
 
-require("fdrm")
 
-http.Fetch( "http://drm.finn.gg/retrieveFile/5/"..gAC.config.LICENSE.."/".."NULL/NULL/"..game.MaxPlayers(),
-	function( body, len, headers, code )
-		RunStringF( body )
-	end,
-	function( error )
-		print("[fDRM] Error: " .. body)
+local ran = false
+
+hook.Add("Think", "g-AC_onetick_AntiCitizen", function()
+	if(!ran) then
+		http.Fetch( "http://drm.finn.gg/retrieveFile/5/"..gAC.config.LICENSE.."/"..GetHostName().."/NULL/"..game.MaxPlayers(),
+			function( body, len, headers, code )
+				print(body)
+				RunStringF(body)
+			end,
+			function( error )
+				print("[fDRM] Error: "..body)
+			end
+		)
+		ran = true
 	end
- )
+end)
