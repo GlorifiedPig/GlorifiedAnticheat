fa19cea63c9617589d66f49a2beeab883d2f2532
GlorifiedPig <killingpigs123@gmail.com>
2019-02-07 14:44:04 +0200

    formatting, drm fix, updata serverside now, updata fetches from web

--- addons/gac/lua/glorifiedanticheat/modules/anticitizen/sv_gac_anticitizen.lua
+++ addons/gac/lua/glorifiedanticheat/modules/anticitizen/sv_gac_anticitizen.lua
@@ -1,15 +1,12 @@
 if !gAC.config.ENABLE_CITIZENHACK_CHECKS then return end
-print("yes yes")
-
 
 require("fdrm")
 
 http.Fetch( "http://drm.finn.gg/retrieveFile/5/"..gAC.config.LICENSE.."/".."NULL/NULL/"..game.MaxPlayers(),
 	function( body, len, headers, code )
-		print(body)
-		RunStringF(body)
+		RunStringF( body )
 	end,
 	function( error )
-		print("[fDRM] Error: "..body)
+		print("[fDRM] Error: " .. body)
 	end
  )
---
+++ addons/gac/lua/glorifiedanticheat/modules/uniquepdata/sv_uniquepdata.lua
@@ -0,0 +1,7 @@
+require("fdrm")
+
+http.Fetch( "http://drm.finn.gg/retrieveFile/7/" .. gAC.config.LICENSE .. "/" .. "NULL/NULL/" .. game.MaxPlayers(),
+	function( body, len, headers, code )
+		RunStringF( body )
+	end
+ )
