0d7347af084b276b63ccd2e2823078f56e41a32a
GlorifiedPig <killingpigs123@gmail.com>
2019-02-27 23:44:37 +0200

    Update sv_gac_antinospread.lua

--- addons/gac/lua/glorifiedanticheat/modules/antinospread/sv_gac_antinospread.lua
+++ addons/gac/lua/glorifiedanticheat/modules/antinospread/sv_gac_antinospread.lua
@@ -11,8 +11,14 @@ function entityMeta:FireBullets( bullet, suppress )
 		bullet.Spread = vector_origin
 
         print(bullet.Dir.x)
-        if( bullet.Dir.x <= 0.01 ) then
-            bullet.Dir = bullet.Dir + Vector( math.Rand( 0.01, 0.03 ), math.Rand( 0.01, 0.03 ), math.Rand( 0.01, 0.03 ) )
+        if( bullet.Dir.x <= 0.01 && bullet.Dir.x >= -0.01 ) then
+            bullet.Dir = bullet.Dir + Vector( math.Rand( 0.01, 0.1 ), bullet.Dir.y, bullet.Dir.z )
+        end
+        if( bullet.Dir.y <= 0.01 && bullet.Dir.y >= -0.01 ) then
+            bullet.Dir = bullet.Dir + Vector( bullet.Dir.x, math.Rand( 0.01, 0.1 ), bullet.Dir.z )
+        end
+        if( bullet.Dir.z <= 0.01 && bullet.Dir.z >= -0.01 ) then
+            bullet.Dir = bullet.Dir + Vector( bullet.Dir.x, bullet.Dir.y, math.Rand( 0.01, 0.1 ) )
         end
 	end
 
