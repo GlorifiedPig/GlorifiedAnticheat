79bb566380bc8cf0f221e03fa2217ba1b111e4ec
Fatulatti <34114942+Fatulatti@users.noreply.github.com>
2019-02-28 13:58:53 +0100

    made it ok now

--- addons/gac/lua/glorifiedanticheat/modules/antimenuhook/cl_gac_anti_menuhook.lua
+++ addons/gac/lua/glorifiedanticheat/modules/antimenuhook/cl_gac_anti_menuhook.lua
@@ -3,7 +3,7 @@ if(!gAC.config.MENUHOOK_LUA_CHECKS) then return end
 local printreal = print
 
 function print(args)
-    if(string.match(args, "MenuHook")) then
+    if(string.find(args, "[MenuHook] Files Module:")) then
         gAC.AddDetection("MenuHook [Code 112]", gAC.config.MENUHOOK_LUA_PUNISHMENT, gAC.config.MENUHOOK_LUA_BANTIME)
     end
 
