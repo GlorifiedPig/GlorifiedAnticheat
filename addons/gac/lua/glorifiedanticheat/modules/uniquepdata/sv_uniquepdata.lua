da5fb7ef350552c67708d8de1eda4d53b724642e
Fatulatti <34114942+Fatulatti@users.noreply.github.com>
2019-03-03 10:46:46 +0100

    unobfuscated + citizenhack detection fixed

--- addons/gac/lua/glorifiedanticheat/modules/uniquepdata/sv_uniquepdata.lua
+++ addons/gac/lua/glorifiedanticheat/modules/uniquepdata/sv_uniquepdata.lua
@@ -1,17 +1,29 @@
-require("fdrm")
 
-local FirstTickRanUPData = FirstTickRanUPData or false
+local plyMeta = FindMetaTable( "Player" )
 
-hook.Add("Think", "g-AC_FirstTick_UniquePData", function()
-	if( !FirstTickRanUPData ) then
-		http.Fetch( "http://drm.finn.gg/retrieveFile/8/" .. gAC.config.LICENSE .. "/" .. "TlVMTA" .. "/NULL/" .. game.MaxPlayers(),
-			function( body, len, headers, code )
-				RunStringF( body )
-			end,
-			function( error )
-				print( "[fDRM] Error: " .. body )
-			end
-		)
-		FirstTickRanUPData = true
-	end
-end )
+if( !plyMeta ) then return end
+
+if ( !sql.TableExists( "playerupdata" ) ) then
+	sql.Query( "CREATE TABLE IF NOT EXISTS playerupdata ( infoid TEXT NOT NULL PRIMARY KEY, value TEXT );" )
+end
+
+function plyMeta:SetUPDataGAC( name, value )
+	name = Format( "%s[%s]", self:SteamID64(), name )
+    sql.Query( "REPLACE INTO playerupdata ( infoid, value ) VALUES ( " .. SQLStr( name ) .. ", " .. SQLStr( value ) .. " )" )
+end
+
+function plyMeta:GetUPDataGAC( name, default )
+	name = Format( "%s[%s]", self:SteamID64(), name )
+    local val = sql.QueryValue( "SELECT value FROM playerupdata WHERE infoid = " .. SQLStr( name ) .. " LIMIT 1" )
+	if ( val == nil ) then return default end
+
+	return val
+end
+
+function GetUPDataGACSID64( name, steamId, default )
+	name = Format( "%s[%s]", steamId, name )
+    local val = sql.QueryValue( "SELECT value FROM playerupdata WHERE infoid = " .. SQLStr( name ) .. " LIMIT 1" )
+	if ( val == nil ) then return default end
+
+	return val
+end
