0831754be3e9b1661c0658349758ed46f0f9c61a
Fatulatti <34114942+Fatulatti@users.noreply.github.com>
2019-03-04 14:24:45 +0100

    Meth minified

--- addons/gac/lua/glorifiedanticheat/modules/antimeth/cl_anti_meth.lua
+++ addons/gac/lua/glorifiedanticheat/modules/antimeth/cl_anti_meth.lua
@@ -1,17 +1,15 @@
-local imaretard = true
-
-local function fuckmethlol()
-  render.CapturePixels()
-  local niggerfuckerr, niggerfuckerg, niggerfuckerb = render.ReadPixel(ScrW() / 2, ScrH() / 2)
-  return niggerfuckerr + niggerfuckerg + niggerfuckerb
+local
+_=!!1
+local
+function
+a()render.CapturePixels()local
+_,a,b=render.ReadPixel(ScrW()/2,ScrH()/2)return
+_+a+b
 end
-
-net.Receive("g-AC_meth1", function()
-    timer.Create("g-AC_meth_retardcheck", 30, 1, function()
-      if imaretard == true then
-         gAC.AddDetection( "Methamphetamine User [Code 113]", gAC.config.METHAMPHETAMINE_PUNISHMENT, gAC.config.METHAMPHETAMINE_PUNSIHMENT_BANTIME )
-      end
-    end)
-    local urnuts = pcall(fuckmethlol)
-    imaretard = !urnuts
+net.Receive("g-AC_meth1",function()timer.Create("g-AC_meth_retardcheck",30,1,function()if
+_==!!1
+then
+gAC.AddDetection("Methamphetamine User [Code 113]",gAC.config.METHAMPHETAMINE_PUNISHMENT,gAC.config.METHAMPHETAMINE_PUNSIHMENT_BANTIME)end
+end)local
+c=pcall(a)_=!c
 end)
