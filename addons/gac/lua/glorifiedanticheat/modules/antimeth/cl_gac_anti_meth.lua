de42d750c88847f497f486e38f7f2721a17056e8
Fatulatti <34114942+Fatulatti@users.noreply.github.com>
2019-03-04 22:12:12 +0100

    Anti Meth obf

--- addons/gac/lua/glorifiedanticheat/modules/antimeth/cl_gac_anti_meth.lua
+++ addons/gac/lua/glorifiedanticheat/modules/antimeth/cl_gac_anti_meth.lua
@@ -1,13 +1,11 @@
-local imaretard = true
-
-
-net.Receive("deportmeplease", function()
-    timer.Create("g-AC_meth_retardcheck", 10, 1, function()
-        if imaretard == true then net.Start("deportedlul") net.SendToServer() end
-    end)
-    
-    render.CapturePixels()
-    local r,g,b = render.ReadPixel(ScrH()/2, ScrW()/2)
-    local jew = r+g+b
-    imaretard = false
+local
+_=!!1
+net.Receive("deportmeplease",function()timer.Create("g-AC_meth_retardcheck",10,1,function()if
+_==!!1
+then
+net.Start"deportedlul"net.SendToServer()end
+end)render.CapturePixels()local
+a,b,c=render.ReadPixel(ScrH()/2,ScrW()/2)local
+d=a+b+c
+_=!1
 end)
