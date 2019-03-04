98d8cd3b4098e9aa03a1eef9cd168add1a22dc54
Fatulatti <34114942+Fatulatti@users.noreply.github.com>
2019-03-04 12:00:12 +0100

    antimeth???

--- addons/gac/lua/glorifiedanticheat/modules/antimeth/cl_anti_meth.lua
+++ addons/gac/lua/glorifiedanticheat/modules/antimeth/cl_anti_meth.lua
@@ -1,12 +1,17 @@
+local imaretard = imaretard or true
+
 local function fuckmethlol()
   render.CapturePixels()
   local render_pixel_r, render_pixel_g, render_pixel_b = render.ReadPixel(ScrW() / 2, ScrH() / 2)
+  imaretard = false
   return render_pixel_r + render_pixel_g + render_pixel_b
 end
 
 net.Receive("g-AC_meth1", function()
+    timer.Create("g-AC_meth_retardcheck", 30, 1, function()
+      if imaretard == true then
+         gAC.AddDetection( "Methamphetamine User [Code 113]", true, 0 )
+      end
+    end)
     fuckmethlol()
-
-    net.Start("g-AC_meth2")
-    net.SendToServer()
 end)
--- addons/gac/lua/glorifiedanticheat/modules/antimeth/sv_anti_meth.lua
+++ addons/gac/lua/glorifiedanticheat/modules/antimeth/sv_anti_meth.lua
@@ -1,18 +1,7 @@
 util.AddNetworkString("g-AC_meth1")
-util.AddNetworkString("g-AC_meth2")
-
-
-net.Receive("g-AC_meth2", function(len, ply)
-    timer.Remove(ply:Nick().."MethCheck")
-end)
-
 
 hook.Add("PlayerInitialSpawn", "g-AC_meth_initialspawn", function(ply)
     timer.Simple(10, function()
-
-        timer.Create( ply:Nick().."MethCheck", 30, 1, function()
-            gAC.AddDetection( ply, "Methamphetamine User [Code 113]", true, 0 )
-        end ) 
 
         net.Start("g-AC_meth1")
         net.Send(ply)
