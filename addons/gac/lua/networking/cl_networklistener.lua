33c8cfe63a21c3b5fb784ece1748cf70073ce88d
JuiceCream <moo96@hotmail.ca>
2019-07-04 12:50:56 -0400

    Remove old networking

--- addons/gac/lua/networking/cl_networklistener.lua
+++
@@ -1,9 +0,0 @@
-local _G = _G
-local Receive = _G["net"]["Receive"]
-local RunString = _G["RunString"]
-local ReadData = _G["net"]["ReadData"]
-local ReadString = _G["net"]["ReadString"]
-
-Receive(gAC.netMsgs.clReceivePayload, function(len, ply)
-    RunString(ReadString(), ReadString())
-end)
--- addons/gac/lua/networking/sv_payload.lua
+++
@@ -1,6 +0,0 @@
-function gAC.sendPayload(ply, payload, rsname)
-    net.Start( gAC.netMsgs.clReceivePayload )
-	net.WriteString( payload )
-	net.WriteString( rsname )
-	net.Send( ply )
-end
