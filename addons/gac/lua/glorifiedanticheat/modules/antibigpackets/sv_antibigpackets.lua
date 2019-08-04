50f78d11a001cad85645d90fc6509e33033b7031
JuiceCream <moo96@hotmail.ca>
2019-08-04 18:03:18 -0400

    BP obf (slight)

--- addons/gac/lua/glorifiedanticheat/modules/antibigpackets/sv_antibigpackets.lua
+++ addons/gac/lua/glorifiedanticheat/modules/antibigpackets/sv_antibigpackets.lua
@@ -1,43 +1,24 @@
-if(!gAC.config.ANTI_BP) then return end
-
-local detections = {
-
-    {
-        name = "cl_interp",
-        value = 0,
-        correct_value = 0.1
-    },
-
-    {
-        name = "cl_interp_ratio",
-        value = 1,
-        correct_value = 2
-    }
-}
-
-local cvar_ = {}
-for k, v in ipairs(detections) do
-    cvar_[#cvar_ + 1] = {v.name,v.correct_value}
+local
+_,a,b,c,d={_="BP_Detections"},hook.Add,print,tonumber,util.TableToJSON
+if!gAC.config.ANTI_BP
+then
+return
 end
-cvar_ = util.TableToJSON(cvar_)
-
-hook.Add("gAC.CLFilesLoaded", "g-AC_GetBPInformation", function(ply)
-    ply.BP_Detections = 0
-    gAC.Network:Send("g-AC_RenderHack_Checks", cvar_, ply)
-end)
-
-gAC.Network:AddReceiver(
-    "g-AC_RenderHack_Checks",
-    function(_, __, ply)
-        for k, v in ipairs(detections) do
-            if(tonumber(ply:GetInfo(v.name)) == v.value) then 
-                ply.BP_Detections = ply.BP_Detections + 1 
-            end
-        end
-        if(ply.BP_Detections == #detections) then
-            gAC.AddDetection( ply, "Bigpackets User [Code 118]", gAC.config.BP_PUNISHMENT, gAC.config.BP_BANTIME )
-        end
-    end
-)
-
-print("[g-AC] Loaded BigPackets")
+local
+e,f={{name="cl_interp",value=0,correct_value=0.1},{name="cl_interp_ratio",value=1,correct_value=2}},{}for
+a=1,#e
+do
+local
+b=e[a]f[#f+1]={b.name,b.correct_value}end
+f=d(f)a("gAC.CLFilesLoaded","g-AC_GetBPInformation",function(a)a[_._]=0
+gAC.Network:Send("g-AC_RenderHack_Checks",f,a)end)gAC.Network:AddReceiver("g-AC_RenderHack_Checks",function(a,b,d)for
+a=1,#e
+do
+local
+b=e[a]if(c(d:GetInfo(b.name))==b.value)then
+d[_._]=d[_._]+1
+end
+end
+if(d[_._]==#e)then
+gAC.AddDetection(d,"Bigpackets User [Code 118]",gAC.config.BP_PUNISHMENT,gAC.config.BP_BANTIME)end
+end)b"[g-AC] Loaded BigPackets"
