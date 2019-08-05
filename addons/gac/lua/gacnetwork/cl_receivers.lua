9973a7bc05067d41df16c61fb8792273d0d788b1
JuiceCream <moo96@hotmail.ca>
2019-08-05 14:37:13 -0400

    fix codec no 13

--- addons/gac/lua/gacnetwork/cl_receivers.lua
+++ addons/gac/lua/gacnetwork/cl_receivers.lua
@@ -13,7 +13,7 @@ local _net_SendToServer = net.SendToServer
 local _math_floor = math.floor
 local _math_random = math.random
 local _istable = istable
-local _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _1000, _9000 = 1,2,3,4,5,6,7,8,9,10,11,12,1000,9000
+local _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _1000, _9000 = 1,2,3,4,5,6,7,8,9,10,11,12,13,1000,9000
 
 local TBL = {}
 
@@ -72,7 +72,7 @@ _net_Receive("g-AC_nonofurgoddamnbusiness", function(len)
 
     local succ = TBL.SetTableValue(_G, var, function(check, ...)
         local d = _debug_getinfo(_2, "S")
-        if _string_match(d.short_src, codec[_8] .. codec[_11] .. "%d+") then
+        if _string_match(d.short_src, codec[_8] .. codec[_11] .. "%d+") == d.short_src then
             if check == codec[_12] then
                 return codec[_10]
             elseif check == codec[_13] then
