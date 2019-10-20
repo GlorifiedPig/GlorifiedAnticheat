bef6913a4fde26ef427ecb80e33a611e60ad93e2
JuiceCream <moo96@hotmail.ca>
2019-10-20 15:05:01 -0400

    fuck nuggets

---
+++ addons/gac/lua/glorifiedanticheat/modules/antiantiaim/sv_gac_antiantiaim.lua
@@ -0,0 +1,43 @@
+local _CurTime = CurTime
+local _IsValid = IsValid
+local _hook_Add = hook.Add
+local _math_abs = math.abs
+
+local Blacklisted_Weapons = {
+    ["weapon_physgun"] = true,
+    ["gmod_tool"] = true,
+    ["weapon_physcannon"] = true,
+    ["gmod_camera"] = true
+}
+
+_hook_Add( "StartCommand", "Bad", function( ply, cmd )
+
+    if( ply:InVehicle() || ply.gAC_AimbotDetected || !ply:Alive() || ply:GetObserverMode() != OBS_MODE_NONE
+    || ply:IsBot() || !_IsValid( ply ) || ply:IsTimingOut() || ply:PacketLoss() > 80 ) then return end
+
+    if( ply.JoinTimeGAC == nil || !( _CurTime() >= ply.JoinTimeGAC + 25 ) || ply.PlayerFullyAuthenticated != true ) then return end
+
+    if _IsValid(ply:GetActiveWeapon()) && Blacklisted_Weapons[ply:GetActiveWeapon():GetClass()] then
+        return 
+    end
+
+    if !ply.Meth_HVH_Threshold then
+        ply.Meth_HVH_Threshold = 0
+    end
+
+    local gAC_View = cmd:GetViewAngles()
+    local p, y, r = gAC_View.p, gAC_View.y, gAC_View.r
+
+
+    if p > 180 or p < -180 or y > 180 or y < -180 or r > 180 or r < -180 then
+        if ply.Meth_HVH_Threshold > 20 then
+            ply.gAC_AimbotDetected = true
+            gAC.AddDetection( ply, "Anti-Aim Detected", false, -1 )
+            return
+        else
+            ply.Meth_HVH_Threshold = ply.Meth_HVH_Threshold + 1
+        end
+    elseif ply.Meth_HVH_Threshold > 0 then
+        ply.Meth_HVH_Threshold = ply.Meth_HVH_Threshold - 1
+    end
+end )
---
+++ addons/gac/lua/glorifiedanticheat/modules/antimethv4/sv_gac_anti_meth_silent.lua
@@ -0,0 +1,67 @@
+local _CurTime = CurTime
+local _IsValid = IsValid
+local _hook_Add = hook.Add
+local _math_abs = math.abs
+
+local Blacklisted_Weapons = {
+    ["weapon_physgun"] = true,
+    ["gmod_tool"] = true,
+    ["weapon_physcannon"] = true,
+    ["gmod_camera"] = true
+}
+
+local function floor(number)
+    return number - (number % 1)
+end
+
+local function round(number, idp)
+	local mult = 10 ^ ( idp or 0 )
+	return floor( number * mult + .5 ) / mult
+end
+
+local function roundangle(ang, idp)
+	ang.p = round(ang.p, idp)
+    ang.y = round(ang.y, idp)
+    ang.r = round(ang.r, idp)
+    return ang
+end
+
+_hook_Add( "StartCommand", "Bad", function( ply, cmd )
+    if( ply:InVehicle() || ply.gAC_AimbotDetected || !ply:Alive() || ply:GetObserverMode() != OBS_MODE_NONE
+    || ply:IsBot() || !_IsValid( ply ) || ply:IsTimingOut() || ply:PacketLoss() > 80 ) then return end
+    
+    if _IsValid(ply:GetActiveWeapon()) && Blacklisted_Weapons[ply:GetActiveWeapon():GetClass()] then
+        return 
+    end
+
+    local gAC_View = cmd:GetViewAngles()
+    local p, y, r = gAC_View.p, gAC_View.y, gAC_View.r
+    local gAC_MX_AB = _math_abs( cmd:GetMouseX() )
+    local gAC_MY_AB = _math_abs( cmd:GetMouseY() )
+
+    if !ply.Meth_Silent_Last then
+        ply.Meth_Silent_Last = gAC_View
+        ply.Meth_Silent_MX_Last = gAC_MX_AB
+        ply.Meth_Silent_MY_Last = gAC_MY_AB
+        ply.Meth_Silent_Threshold = 0
+        return
+    end
+
+    local rounded_oldview, rounded_newview = roundangle(ply.Meth_Silent_Last), roundangle(gAC_View)
+
+    if gAC_MX_AB > 0 and gAC_MY_AB > 0 and roundangle(rounded_oldview, 2) == roundangle(rounded_newview, 2) then
+        if ply.Meth_Silent_Threshold > 5 then
+            ply.gAC_AimbotDetected = true
+            gAC.AddDetection( ply, "Meth Silent-Aim Detected", false, -1 )
+            return
+        else
+            ply.Meth_Silent_Threshold = ply.Meth_Silent_Threshold + 1
+        end
+    elseif ply.Meth_Silent_Threshold > 0 then
+        ply.Meth_Silent_Threshold = ply.Meth_Silent_Threshold - 1
+    end
+
+    ply.Meth_Silent_Last = gAC_View
+    ply.Meth_Silent_MX_Last = gAC_MX_AB
+    ply.Meth_Silent_MY_Last = gAC_MY_AB
+end )
