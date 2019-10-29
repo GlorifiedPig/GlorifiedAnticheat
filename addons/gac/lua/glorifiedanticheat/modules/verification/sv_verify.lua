350caa2570395b008db26120fac55d62a2e6457a
JuiceCream <moo96@hotmail.ca>
2019-10-28 20:01:26 -0400

    sync

--- addons/gac/lua/glorifiedanticheat/modules/verification/sv_verify.lua
+++ addons/gac/lua/glorifiedanticheat/modules/verification/sv_verify.lua
@@ -19,7 +19,15 @@ if !gAC.config.INTEGRITY_CHECKS then return end
 
 local Configs = {}
 
+local Blocked_Cfgs = {
+    ['STEAM_API_KEY'] = true,
+    ['ENABLE_FAMILY_SHARE_CHECKS'] = true,
+    ['FAMILY_SHARE_PUNISHMENT'] = true,
+    ['FAMILY_SHARE_BANTIME'] = true
+}
+
 for k, v in _pairs(gAC.config) do
+    if Blocked_Cfgs[k] then continue end
     if _isbool(v) or _isnumber(v) then
         Configs[k] = v
     end
