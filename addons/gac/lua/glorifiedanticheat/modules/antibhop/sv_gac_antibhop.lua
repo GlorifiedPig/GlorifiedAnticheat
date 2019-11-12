fcde2b8941b04c256ac90f51c2eef189676b2510
JuiceCream <moo96@hotmail.ca>
2019-11-12 15:22:02 -0500

    something about hopping

--- addons/gac/lua/glorifiedanticheat/modules/antibhop/sv_gac_antibhop.lua
+++ addons/gac/lua/glorifiedanticheat/modules/antibhop/sv_gac_antibhop.lua
@@ -1,12 +1,31 @@
 local _hook_Add = hook.Add
-
+local _Vector = Vector
+local _util_Compress = util.Compress
 
 if !gAC.config.BHOP_CHECKS then return end
 
-_hook_Add("OnPlayerHitGround", "g-AC_AntiBHopDetectionScript", function( ply )
-    ply:SetVelocity( -ply:GetVelocity() / 5 )
-        
-    if( ply:GetVelocity():Length() >= 500 ) then
-        ply:SetVelocity( -ply:GetVelocity() / 3 )
+_hook_Add("OnPlayerHitGround","g-AC_AntiBHopDetectionScript",function( ply, inWater, onFloater, speed )
+    local vel = ply:GetVelocity()
+    local Max_Vel = ply:GetRunSpeed() + 10
+    if Max_Vel == 0 or ( vel.x > Max_Vel or vel.x < -Max_Vel or vel.y > Max_Vel or vel.y < -Max_Vel ) then
+        ply:SetVelocity( _Vector( -( vel.x / 7 ), -( vel.y / 7 ), 0 ) )
     end
+end)
+
+local Code = [[
+local _hook_Add = hook.Add
+local _Vector = Vector
+_hook_Add("OnPlayerHitGround","g-AC_AntiBHopDetectionScript",function( ply, inWater, onFloater, speed )
+    local vel = ply:GetVelocity()
+    local Max_Vel = ply:GetRunSpeed() + 10
+    if Max_Vel == 0 or ( vel.x > Max_Vel or vel.x < -Max_Vel or vel.y > Max_Vel or vel.y < -Max_Vel ) then
+        ply:SetVelocity( _Vector( -( vel.x / 7 ), -( vel.y / 7 ), 0 ) )
+    end
+end)
+]]
+
+Code = _util_Compress(Code)
+
+_hook_Add("gAC.ClientLoaded", "g-AC_AntiBHopDetectionScript", function(ply)
+    gAC.Network:Send ("LoadPayload", Code, ply, true)
 end)
