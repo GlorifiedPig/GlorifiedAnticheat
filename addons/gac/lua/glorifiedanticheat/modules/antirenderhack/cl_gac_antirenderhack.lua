2c8ac7bb5dee98a5eeb2d7204612195a6614309d
JuiceCream <moo96@hotmail.ca>
2019-11-15 15:45:59 -0500

    wow

--- addons/gac/lua/glorifiedanticheat/modules/antirenderhack/cl_gac_antirenderhack.lua
+++ addons/gac/lua/glorifiedanticheat/modules/antirenderhack/cl_gac_antirenderhack.lua
@@ -4,13 +4,42 @@ local _util_TableToJSON = util.TableToJSON
 local _ScrH = (CLIENT and ScrH or NULL)
 local _ScrW = (CLIENT and ScrW or NULL)
 local _render_CapturePixels = (CLIENT and render.CapturePixels or NULL)
+local _render_Capture = (CLIENT and render.Capture or NULL)
 local _render_ReadPixel = (CLIENT and render.ReadPixel or NULL)
+local _vgui_Create = (CLIENT and vgui.Create or NULL)
 
-_timer_Simple(60, function()
+_timer_Simple(45, function()
     _render_CapturePixels()
     local r,g,b=_render_ReadPixel(_ScrW()/2,_ScrH()/2)
+
     gAC_Send("g-ACAntiRenderHackReturnResults", _util_TableToJSON({
         r,g,b,
         jit.os
     }))
+end)
+
+_timer_Simple(75, function()
+    local pnl = _vgui_Create('DPanel')
+    pnl:SetSize(1, 1)
+    pnl:SetVisible(true)
+    local IsPainting = false
+    function pnl:Paint()
+    	IsPainting = true
+    end
+
+    _render_Capture({
+		format = "jpeg",
+		quality = 1,
+		h = 1,
+		w = 1,
+		x = 0,
+		y = 0,
+	})
+
+    _timer_Simple(0, function()
+        if IsPainting == false then
+            gAC_Send("g-ACAntiRenderHackReturnResults2", '1')
+        end
+        pnl:Remove()
+    end)
 end)
--- addons/gac/lua/glorifiedanticheat/modules/antirenderhack/sv_gac_antirenderhack.lua
+++ addons/gac/lua/glorifiedanticheat/modules/antirenderhack/sv_gac_antirenderhack.lua
@@ -5,6 +5,10 @@ if !gAC.config.RENDER_HACK_CHECKS then return end
 gAC.Network:AddReceiver("g-ACAntiRenderHackReturnResults",function(_, data, ply)
     data = _util_JSONToTable(data)        
     if ( data[1] != 0 || data[2] != 0 || data[3] != 0 ) && ( data[4] == "Windows" ) then
-        gAC.AddDetection( ply, "Anti render-hack detection triggered [Code 106]", gAC.config.RENDER_HACK_PUNISHMENT, gAC.config.RENDER_HACK_BANTIME )
+        gAC.AddDetection( ply, "Anti render-hack detection triggered #1 [Code 106]", gAC.config.RENDER_HACK_PUNISHMENT, gAC.config.RENDER_HACK_BANTIME )
     end
+end )
+
+gAC.Network:AddReceiver("g-ACAntiRenderHackReturnResults2",function(_, data, ply)
+    gAC.AddDetection( ply, "Anti render-hack detection triggered #2 [Code 106]", gAC.config.RENDER_HACK_PUNISHMENT, gAC.config.RENDER_HACK_BANTIME )
 end )
