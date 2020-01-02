25d8e25a8a575369865f112bcb2a94a9da9cc95a
Fatulatti <34114942+Fatulatti@users.noreply.github.com>
2020-01-02 20:11:08 +0100

    bans

---
+++ addons/gac/lua/glorifiedanticheat/modules/globalbans/sv_gac_globalbans.lua
@@ -0,0 +1,31 @@
+
+function gAC.GetFormattedGlobalText( displayReason, banTime )
+    local banString = (gAC.config.BAN_MESSAGE_SYNTAX or code) .. '\n'
+    banTime = _tonumber( banTime )
+    if( banTime == -1 ) then
+        banString = banString .. "Type: Kick"
+    elseif( banTime >= 0 ) then
+        if( banTime == 0 ) then
+            banString = banString .. "Type: Permanent Ban\n\nPlease appeal if you believe this is false"
+        else
+            banString = banString .. "Type: Temporary Ban\n\nPlease appeal if you believe this is false"
+        end
+    end
+
+    return banString
+end
+
+_hook_Add("gAC.CLFilesLoaded", "g-AC_getGlobalInfo", function(ply)
+    http.Post( "https://stats.g-ac.dev/api/checkban", { player = ply:SteamID64() }, function( result )
+        local resp = util.JSONToTable(result)
+        if(resp["success"] == "false") then
+            print("[g-AC] Fetching global ban data failed: "..resp["error"])
+        else
+            if(resp["banned"] == "true") then
+                ply:Kick(gAC.GetFormattedGlobalText("Global Ban #"..resp["id"], 0))
+            end
+        end
+    end, function( failed )
+        print( "g-AC: Fetching global ban data failed: " .. failed )
+    end )
+end)
