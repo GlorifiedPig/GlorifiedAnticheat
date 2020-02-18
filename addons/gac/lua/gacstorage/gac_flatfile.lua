543fe5dc950dbdd8f9fb756ee99a8b404452dd48
eprosync <38162078+eprosync@users.noreply.github.com>
2020-02-18 10:38:01 -0800

    s

--- addons/gac/lua/gacstorage/gac_flatfile.lua
+++ addons/gac/lua/gacstorage/gac_flatfile.lua
@@ -4,13 +4,16 @@ local _file_IsDir = file.IsDir
 local _file_Write = file.Write
 
 function gAC.LogEvent( ply, str )
-    local eventLogFile = "g-ac-logs/eventlogs.txt"
-    if !_file_IsDir( "g-ac-logs", "DATA" ) then
-        _file_CreateDir( "g-ac-logs", "DATA" )
-
-        _file_Write( eventLogFile, "[" .. os.date( "%m/%d/%Y: %H:%M:%S", os.time() ) .. "] "  .. ply:Nick() .. " (" .. ply:SteamID() .. ") : " .. str .. "\n" )
+    local ID64 = ply:SteamID64()
+    local time = os.time()
+    local eventLogFolder = 'g-ac-logs/' .. os.date('%d-%m-%Y', time)
+    if !_file_Exists(eventLogFolder, 'DATA') then
+        _file_CreateDir(eventLogFolder)
+    end
+    if _file_Exists(eventLogFolder .. '/' .. ID64 .. ".dat", 'DATA') then
+        _file_Append(eventLogFolder .. '/' .. ID64 .. ".dat", "[" .. os.date( "%m/%d/%Y: %H:%M:%S", time ) .. "] "  .. ply:Nick() .. " (" .. ply:SteamID() .. ") : " .. str .. '\n')
     else
-        _file_Append( eventLogFile, "[" .. os.date( "%m/%d/%Y: %H:%M:%S", os.time() ) .. "] "  .. ply:Nick() .. " (" .. ply:SteamID() .. ") : " .. str .. "\n" )
+        _file_Write(eventLogFolder .. '/' .. ID64 .. ".dat", "[" .. os.date( "%m/%d/%Y: %H:%M:%S", time ) .. "] "  .. ply:Nick() .. " (" .. ply:SteamID() .. ") : " .. str .. '\n')
     end
 end
 
