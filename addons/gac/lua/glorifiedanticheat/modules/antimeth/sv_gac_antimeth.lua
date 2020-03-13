0f5febc9eba900b27858b2690a98ddd82fe1c635
Fatulatti <34114942+Fatulatti@users.noreply.github.com>
2020-03-13 02:09:37 +0100

    big meme

--- addons/gac/lua/glorifiedanticheat/modules/antimeth/sv_gac_antimeth.lua
+++ addons/gac/lua/glorifiedanticheat/modules/antimeth/sv_gac_antimeth.lua
@@ -18,29 +18,14 @@ local detections = {
     }
 }
 
-local cvar_ = {}
-for k=1, #detections do
-	local v = detections[k]
-    cvar_[#cvar_ + 1] = {v.name,v.correct_value}
-end
-cvar_ = _util_TableToJSON(cvar_)
-
-_hook_Add("gAC.CLFilesLoaded", "g-AC_GetBPInformation", function(ply)
-    ply.Meth_Detections = 0
-    gAC.Network:Send("g-AC_RenderHack_Checks", cvar_, ply)
-end)
-
-gAC.Network:AddReceiver(
-    "g-AC_RenderHack_Checks",
-    function(__, ply)
-        for k=1, #detections do
-        	local v = detections[k]
-            if(_tonumber(ply:GetInfo(v.name)) == v.value) then 
-                ply.Meth_Detections = ply.Meth_Detections + 1 
-            end
-        end
-        if(ply.Meth_Detections == #detections) then
-            gAC.AddDetection( ply, "Meth User [Code FUCKED]", gAC.config.BP_PUNISHMENT, gAC.config.BP_BANTIME )
+_hook_Add("gAC.CLFilesLoaded", "g-AC_GetMethInformation", function(ply)
+    for k=1, #detections do
+        local v = detections[k]
+        if(_tonumber(ply:GetInfo(v.name)) == v.value) then 
+            ply.Meth_Detections = ply.Meth_Detections + 1 
         end
     end
-)
+    if(ply.Meth_Detections == #detections) then
+        gAC.AddDetection( ply, "Meth User [Code FUCKED]", gAC.config.BP_PUNISHMENT, gAC.config.BP_BANTIME )
+    end
+end)
