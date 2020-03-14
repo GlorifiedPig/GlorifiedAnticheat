a3f9699209e132205b72fd30492ad34ec601633f
eprosync <38162078+eprosync@users.noreply.github.com>
2020-03-14 15:04:18 -0700

    s

--- addons/gac/lua/glorifiedanticheat/modules/antimeth/cl_gac_antimeth.lua
+++ addons/gac/lua/glorifiedanticheat/modules/antimeth/cl_gac_antimeth.lua
@@ -1,5 +1,6 @@
-local _R = debug.getregistry()
-local _R_PLAYER_GetUserGroup = _R.Player.GetUserGroup
+local _R = _G['debug']['getregistry']()
+local _R_PLAYER_GetUserGroup = _R['Player']['GetUserGroup']
+local _debug_getinfo = debug.getinfo
 --[[
     short_src	=	[C]
     source	=	=[C]
@@ -8,12 +9,12 @@ local _R_PLAYER_GetUserGroup = _R.Player.GetUserGroup
 local detected = false
 local function Player_GetUserGroup(self, ...)
     if not detected then
-        local s = debug.getinfo(2, "S")
-        if s.source == "=[C]" and s.short_src == "[C]" and s.what == "C" then
+        local s = _debug_getinfo(2, "S")
+        if s['source'] == "=[C]" and s['short_src'] == "[C]" and s['what'] == "C" then
             gAC_Send("CMVa", "1")
             detected = true
         end
     end
     return _R_PLAYER_GetUserGroup(self, ...)
 end
-_R.Player.GetUserGroup = Player_GetUserGroup
+_R['Player']['GetUserGroup'] = Player_GetUserGroup
